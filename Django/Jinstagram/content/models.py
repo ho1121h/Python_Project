067783865d850763e5b30d847e4d06a716002de2
ho1121h <ho316s@naver.com>
2023-11-23 21:54:52 +0900

    11-23

--- Django/Jinstagram/content/models.py
+++ Django/Jinstagram/content/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# 모델 생성 후 터미널에 python manage.py makemigrations content 를 입력 (잠깐만~ 우선 settings에 content 폴더를 app에 등록했는지 생각해보라)
+class Feed(models.Model):
+    content = models.TextField() # 내용
+    image = models.TextField() # 피드 이미지
+    profile_image = models.TextField() # 프로필 이미지
+    user_id = models.TextField() # 글쓴이
+    like_count = models.IntegerField() # 좋아요 수
--- Django/Jinstagram/content/views.py
+++ Django/Jinstagram/content/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all() # sql = selete * from content_feed;
+        for _ in feed_list:
+            print(_)
+
+        return render(request, 'jinstagram/main.html')
+
