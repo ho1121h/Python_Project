b3386b8c02f0f5ae4a323d1614650b24aa3158cb
ho1121h <ho316s@naver.com>
2023-12-08 08:22:21 +0900

    django feeds

--- Django/Jinstagram/content/views.py
+++ Django/Jinstagram/content/views.py
@@ -5,9 +5,8 @@ from .models import Feed
 # Create your views here.
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all() # sql = selete * from content_feed;
-        for _ in feed_list:
-            print(_)
+        feed_list = Feed.objects.all().order_by('-id') # sql = selete * from content_feed; / 역순정렬
 
-        return render(request, 'jinstagram/main.html')
+  
+        return render(request, 'jinstagram/main.html', context=dict(feeds=feed_list))
 
